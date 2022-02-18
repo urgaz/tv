cece05ffc49b59fc532edc51fa738d1a50d6f009
xmas1256 <xmas59233@gmail.com>
2022-02-18 16:50:42 +0500

    first

--- urgaz_tutorials/settings.py
+++ urgaz_tutorials/settings.py
@@ -91,7 +91,7 @@ else:
         'default': {
     	'ENGINE': 'django.db.backends.postgresql_psycopg2',
     	'NAME': DB_NAME,
-    	'USER': DB_NAME,
+    	'USER': DB_USER,
     	'PASSWORD': DB_PASSWORD,
     	'HOST': DB_HOST,
     	'PORT': DB_PORT,
